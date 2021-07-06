f7638fffa230f993931dc24cef7a626090dbd358
Joseph VP <jvprakashfs@gmail.com>
2021-07-06 17:06:06 +0530

    Initial Commit

---
+++ src/main/java/com/jose/beer/BeersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jose.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose/beer/BeersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
